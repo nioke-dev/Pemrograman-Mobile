2cdacf71251e2e64385e5af954501b2afc2e6153
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-10-25 23:53:20 +0700

    init project

--- belanja/lib/models/items.dart
+++
@@ -1,7 +0,0 @@
-class Item {
-  final String name;
-  final int price;
-  final String photo;
-
-  Item({required this.name, required this.price, required this.photo});
-}
--- flutter_layout/android/app/src/main/kotlin/com/example/flutter_layout/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_layout
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
